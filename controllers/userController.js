732a0c24f7108fa999222604c82d82cc7939b688
[MelaniePorley] <melanie.porley@gmail.com>
2023-11-15 14:10:26 -0300

    new commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,7 +9,7 @@ const getUserById = (req, res) => {
   if (user) {
     res.status(200).json(user);
   } else {
-    res.status(404).json({ message: "Usuario no encontrado" });
+    res.status(500).json({ message: "Usuario no encontrado" });
   }
 };
 
@@ -25,14 +25,14 @@ const createUser = (req, res) => {
 const updateUser = (req, res) => {
   const updatedUser = userModel.updateUser(req.params.id, req.body);
   if (updatedUser) {
-    res.status(404).json(updatedUser);
+    res.status(200).json(updatedUser);
   } else {
     res.status(404).json({ message: "Usuario no encontrado" });
   }
 };
 
 const deleteUser = (req, res) => {
-  const deletedUser = userModel.deleteUser(req.params.id);
+  const deletedUser = userModel.deleteUser([req.params.id]);
   if (deletedUser) {
     res.status(200).json(deletedUser);
   } else {
